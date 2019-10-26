8b05939740452aa8ff5e7396f0e849a3f89174ed
Aalydon <amattilf@gmail.com>
2019-10-26 14:13:11 -0700

    Spawner working

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnScript : MonoBehaviour
-{
+public class SpawnScript : MonoBehaviour {
     // Start is called before the first frame update
-    void Start() {
-        public GameObject Ipiece;
-        public GameObject Lpiece;
-        public GameObject Lpiece2;
-        public GameObject Opiece;
-        public GameObject Zpiece;
-        public GameObject Zpiece2;
+    public GameObject [] pieceArray;
+    public static Vector3 scale;
+    private float x;
+    private float z;
 
+    public float minInterval;
+
+    void Start() {
+        scale = transform.localScale;
+        StartCoroutine("DoCheck");
+        x = transform.localScale.x;
+        z = transform.localScale.z;
     }
 
     // Update is called once per frame
-    void Update() {
-
-         //while(true){
-
-
-
-        // }
-
+    void Update()
+    {
 
     }
+
+    IEnumerator DoCheck() {
+         for(;;) {
+             spawnObject();// execute block of code here
+             yield return new WaitForSeconds(minInterval);
+         }
+     }
+
+     void spawnObject(){
+             float xr = Random.Range(0.0f,1.0f);
+             float zr = Random.Range(0.0f,1.0f);
+             Vector3 randomPosition = new Vector3 (transform.position.x + xr*x, transform.position.y, transform.position.z + zr*z );
+             print(randomPosition);
+             int r = Random.Range(0,6);
+             Instantiate(pieceArray[r], randomPosition, Quaternion.identity);
+     }
+
 }
