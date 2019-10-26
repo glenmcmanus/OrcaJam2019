881882b21ba9719744548bc2798c10acf55e4f87
Glen <glen.don.mcmanus@gmail.com>
2019-10-25 23:27:20 -0700

    add test scenes; tube examples

---
+++ Assets/Scripts/PlatformPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayer : MonoBehaviour
+{
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Vector2.zero;
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement = Vector2.left;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            movement = Vector2.right;
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            //jump
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            //crouch
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator Run()
+    {
+        //look at ceiling 
+
+        //rotate to top-down
+
+        //rotate to side-view
+
+        //follow player
+        yield return null;
+    }
+}
