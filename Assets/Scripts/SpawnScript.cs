79dfbc50184faa91751b2570b020b4723bbd2bd5
Aalydon <amattilf@gmail.com>
2019-10-26 12:33:04 -0700

    created tetris prefabs

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -6,13 +6,24 @@ public class SpawnScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start() {
-
+        public GameObject Ipiece;
+        public GameObject Lpiece;
+        public GameObject Lpiece2;
+        public GameObject Opiece;
+        public GameObject Zpiece;
+        public GameObject Zpiece2;
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+
+         //while(true){
+
+
+
+        // }
+
 
     }
 }
